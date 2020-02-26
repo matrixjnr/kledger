2516cc699ff6b5aa2815a950cc34a9c0be53a95f
matrixjnr <jonnyeinstein@gmail.com>
2020-02-26 19:28:14 +0300

    Migrations

--- config/config.py
+++ config/config.py
@@ -29,5 +29,5 @@ class StagingConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://xdkqznupmgnkdg:cfa95b4e85f17bb2df9d1af784acc694670cf4bd6a1a5b22ba58faa324a4eef8@ec2-3-213-192-58.compute-1.amazonaws.com:5432/ddrf7lcrug4tpi
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://xdkqznupmgnkdg:cfa95b4e85f17bb2df9d1af784acc694670cf4bd6a1a5b22ba58faa324a4eef8@ec2-3-213-192-58.compute-1.amazonaws.com:5432/ddrf7lcrug4tpi'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
