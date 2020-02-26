915d09d81500ab88db60407a3876a6bd6ac1a320
matrixjnr <jonnyeinstein@gmail.com>
2020-02-26 19:21:26 +0300

    DB changes

--- config/base.py
+++ config/base.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate
 from config.config import DevelopmentConfig
 
 
@@ -13,4 +12,3 @@ app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
-migrate = Migrate(app, db)
